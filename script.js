1244d24da4b4589fb69181ad5a279d0c3af728e2
ekohadipermana <ekohadipermana@gmail.com>
2024-02-14 10:52:50 +0700

    changing the query

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ console.log(showModalBtn);
 for (let i = 0; i < showModalBtn.length; i++) {
     console.log(showModalBtn[i]);
 }
+let modal = document.querySelector('.modal');
+let closeModal = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+
