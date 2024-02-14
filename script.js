74e7056c961a98dc51e6b3b42f05e1c1d191b06e
ekohadipermana <ekohadipermana@gmail.com>
2024-02-14 10:40:45 +0700

    creating query

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+let showModalBtn = document.querySelectorAll('.show-modal');
+console.log(showModalBtn);
+for (let i = 0; i < showModalBtn.length; i++) {
+    console.log(showModalBtn[i]);
+}
