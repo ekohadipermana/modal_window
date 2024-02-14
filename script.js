8f51619b2b3a27596d4eecb76498673dd8425096
ekohadipermana <ekohadipermana@gmail.com>
2024-02-14 15:15:24 +0700

    creating constants

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ let modal = document.querySelector('.modal');
 let closeModal = document.querySelector('.close-modal');
 let overlay = document.querySelector('.overlay');
 
-let buttonClicked = function () {
+const buttonClicked = function () {
     console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
 
+const closeModalWindow = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const overlayClicked = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < showModalBtn.length; i++) {
     showModalBtn[i].addEventListener('click', buttonClicked);
 }
+
+closeModal.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', overlayClicked);
+
 
 
